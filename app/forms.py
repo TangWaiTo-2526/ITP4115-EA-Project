b76663443716f55ea7c9ecd824473d5396ed91f4
Rimon12 <TangChan12300@gmail.com>
2026-03-22 04:13:49 +0800

    重構登入和註冊表單，改進佈局；更新頁首和頁尾樣式以提高美观性；刪除未使用的已編譯 Python 檔案

--- app/forms.py
+++ app/forms.py
@@ -8,30 +8,52 @@ from app.models import User
 
 
 class LoginForm(FlaskForm):
-    username = StringField(_l('Username'), validators=[DataRequired()])
-    password = PasswordField(_l('Password'), validators=[DataRequired()])
-    remember_me = BooleanField(_l('Remember Me'))
-    submit = SubmitField(_l('Sign In'))
+    username = StringField(
+        '使用者名稱',
+        validators=[DataRequired(message='請填寫使用者名稱')],
+    )
+    password = PasswordField(
+        '密碼',
+        validators=[DataRequired(message='請填寫密碼')],
+    )
+    remember_me = BooleanField('記住我')
+    submit = SubmitField('登入')
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField(_l('Username'), validators=[DataRequired()])
-    email = StringField(_l('Email'), validators=[DataRequired(), Email()])
-    password = PasswordField(_l('Password'), validators=[DataRequired()])
+    username = StringField(
+        '使用者名稱',
+        validators=[DataRequired(message='請填寫使用者名稱')],
+    )
+    email = StringField(
+        '電子郵件',
+        validators=[
+            DataRequired(message='請填寫電子郵件'),
+            Email(message='請輸入有效的電子郵件地址'),
+        ],
+    )
+    password = PasswordField(
+        '密碼',
+        validators=[DataRequired(message='請填寫密碼')],
+    )
     password2 = PasswordField(
-        _l('Repeat Password'), validators=[DataRequired(),
-                                           EqualTo('password')])
-    submit = SubmitField(_l('Register'))
+        '確認密碼',
+        validators=[
+            DataRequired(message='請再次輸入密碼'),
+            EqualTo('password', message='兩次輸入的密碼必須相同'),
+        ],
+    )
+    submit = SubmitField('註冊')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError(_('Please use a different username.'))
+            raise ValidationError('此使用者名稱已被使用，請換一個。')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise ValidationError(_('Please use a different email address.'))
+            raise ValidationError('此電子郵件已被註冊，請換一個。')
 
 
 class ResetPasswordRequestForm(FlaskForm):
